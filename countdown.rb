11b3b86734fa90814b5a84cc2b4ad84a2b84f664
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-09 21:39:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+# This took me a long time and I still don't fully understand how this works? - It did make me realize to read instructions carefully!!!
+def countdown(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
